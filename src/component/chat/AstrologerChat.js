9ee4c56d393e1228f93244636c7f9d79da909610
AmmyAstro <amrender@dhwaniastro.com>
2026-09-09 14:41:45 +0530

    website

--- src/component/chat/AstrologerChat.js
+++ src/component/chat/AstrologerChat.js
@@ -790,7 +790,7 @@ const AstrologerChat = () => {
     });
 
     window.open(
-      `https://dhwani-astro-v2.vercel.app/freeservices/kundali/getKundaliPage?${params.toString()}`,
+      `https://dhwani-astro-website-v2.vercel.app/freeservices/kundali/getKundaliPage?${params.toString()}`,
       "_blank",
     );
   };
